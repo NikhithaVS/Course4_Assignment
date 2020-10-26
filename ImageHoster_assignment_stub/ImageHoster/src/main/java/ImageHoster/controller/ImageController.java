6b294735bdf20a1b7c60faeedec8f80b2ac75d69
NikhithaVS <nikhithabharadwaj14@gmail.com>
2020-10-26 20:31:57 +0530

    Fix bug where any user could edit an image uploaded by any other user

--- ImageHoster_assignment_stub/ImageHoster/src/main/java/ImageHoster/controller/ImageController.java
+++ ImageHoster_assignment_stub/ImageHoster/src/main/java/ImageHoster/controller/ImageController.java
@@ -92,7 +92,7 @@ public class ImageController {
     //The method first needs to convert the list of all the tags to a string containing all the tags separated by a comma and then add this string in a Model type object
     //This string is then displayed by 'edit.html' file as previous tags of an image
     @RequestMapping(value = "/editImage")
-    public String editImage(@RequestParam("imageId") Integer imageId, Model model) {
+    public String editImage(@RequestParam("imageId") Integer imageId, HttpSession session, Model model) {
         Image image = imageService.getImage(imageId);
 
         String tags = convertTagsToString(image.getTags());
@@ -113,11 +113,12 @@ public class ImageController {
     //The method also receives tags parameter which is a string of all the tags separated by a comma using the annotation @RequestParam
     //The method converts the string to a list of all the tags using findOrCreateTags() method and sets the tags attribute of an image as a list of all the tags
     @RequestMapping(value = "/editImage", method = RequestMethod.PUT)
-    public String editImageSubmit(@RequestParam("file") MultipartFile file, @RequestParam("imageId") Integer imageId, @RequestParam("tags") String tags, Image updatedImage, HttpSession session) throws IOException {
+    public String editImageSubmit(@RequestParam("file") MultipartFile file, @RequestParam("imageId") Integer imageId, @RequestParam("tags") String tags, Image updatedImage, HttpSession session, Model model) throws IOException {
 
         Image image = imageService.getImage(imageId);
         String updatedImageData = convertUploadedFileToBase64(file);
         List<Tag> imageTags = findOrCreateTags(tags);
+        User user = (User) session.getAttribute("loggeduser");
 
         if (updatedImageData.isEmpty())
             updatedImage.setImageFile(image.getImageFile());
@@ -126,13 +127,19 @@ public class ImageController {
         }
 
         updatedImage.setId(imageId);
-        User user = (User) session.getAttribute("loggeduser");
         updatedImage.setUser(user);
         updatedImage.setTags(imageTags);
         updatedImage.setDate(new Date());
 
-        imageService.updateImage(updatedImage);
         if (image.getUser() != user) {
+            String error = "Only the owner of the image can edit the image";
+            model.addAttribute("editError", error);
+            List<Comment> comments = commentService.getCommentsByImageId(imageId);
+            model.addAttribute("comments", comments);
+            return "images/image";
+        } else {
+            imageService.updateImage(updatedImage);
+        }
         return "redirect:/images/" + updatedImage.getId() + "/" + updatedImage.getTitle();
     }
 
